8e757c0321991efc3fff12fbfb3835630a7b7f32
Leonhard Lichtschlag <leonhard.lichtschlag@rwth-aachen.de>
2012-02-12 04:28:35 +0100

    more bling bling by emitting emitters from the emitters

--- Dazzle/DazTouchController.m
+++ Dazzle/DazTouchController.m
@@ -43,28 +43,65 @@
 	self.ringEmitter.emitterSize	= CGSizeMake(50, 0);
 	self.ringEmitter.emitterMode	= kCAEmitterLayerOutline;
 	self.ringEmitter.emitterShape	= kCAEmitterLayerCircle;
-	self.ringEmitter.renderMode		= kCAEmitterLayerAdditive;
+	self.ringEmitter.renderMode		= kCAEmitterLayerBackToFront;
 		
 	// Create the fire emitter cell
 	CAEmitterCell* ring = [CAEmitterCell emitterCell];
 	[ring setName:@"ring"];
 	
 	ring.birthRate			= 0;
-	//	ring.emissionLongitude  = M_PI;
 	ring.velocity			= 250;
-	//	ring.velocityRange		= 0;
-	//	ring.emissionRange		= M_PI;
-	//	ring.yAcceleration		= -200;
-	ring.scale				= 0.3;
-	ring.scaleSpeed			= 0.3;
+	ring.scale				= 0.5;
+	ring.scaleSpeed			=-0.2;
+	ring.greenSpeed			=-0.2;
+	ring.redSpeed			=-0.5;
+	ring.blueSpeed			=-0.5;
 	ring.lifetime			= 2;
-	//	ring.lifetimeRange		= 0;
 	
 	ring.color = [[UIColor whiteColor] CGColor];
-	ring.contents = (id) [[UIImage imageNamed:@"DazStarOutline"] CGImage];
-		
+	ring.contents = (id) [[UIImage imageNamed:@"DazTriangle"] CGImage];
+	
+
+	CAEmitterCell* circle = [CAEmitterCell emitterCell];
+	[circle setName:@"circle"];
+	
+	circle.birthRate		= 10;
+	circle.emissionLongitude = M_PI * 0.5;
+	circle.velocity			= 50;
+	circle.scale			= 0.5;
+	circle.scaleSpeed		=-0.2;
+	circle.greenSpeed		=-0.1;
+	circle.redSpeed			=-0.2;
+	circle.blueSpeed		= 0.1;
+	circle.alphaSpeed		=-0.2;
+	circle.lifetime			= 4;
+	
+	circle.color = [[UIColor whiteColor] CGColor];
+	circle.contents = (id) [[UIImage imageNamed:@"DazRing"] CGImage];
+
+
+	CAEmitterCell* star = [CAEmitterCell emitterCell];
+	[star setName:@"star"];
+	
+	star.birthRate		= 10;
+	star.velocity		= 100;
+	star.zAcceleration  = -1;
+	star.emissionLongitude = -M_PI;
+	star.scale			= 0.5;
+	star.scaleSpeed		=-0.2;
+	star.greenSpeed		=-0.1;
+	star.redSpeed		= 0.4;
+	star.blueSpeed		=-0.1;
+	star.alphaSpeed		=-0.2;
+	star.lifetime		= 2;
+	
+	star.color = [[UIColor whiteColor] CGColor];
+	star.contents = (id) [[UIImage imageNamed:@"DazStarOutline"] CGImage];
+	
 	// Putting things together
 	self.ringEmitter.emitterCells = [NSArray arrayWithObject:ring];
+	ring.emitterCells = [NSArray arrayWithObjects:circle, star, nil];
+	//	circle.emitterCells = [NSArray arrayWithObject:star];
 	[self.view.layer addSublayer:self.ringEmitter];
 }
 
@@ -108,7 +145,7 @@
 {
 	// Bling bling..
 	CABasicAnimation *burst = [CABasicAnimation animationWithKeyPath:@"emitterCells.ring.birthRate"];
-	burst.fromValue			= [NSNumber numberWithFloat: 25.0];
+	burst.fromValue			= [NSNumber numberWithFloat: 125.0];
 	burst.toValue			= [NSNumber numberWithFloat: 0.0];
 	burst.duration			= 0.5;
 	burst.timingFunction	= [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
